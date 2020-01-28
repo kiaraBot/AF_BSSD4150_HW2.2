b1da78628b9693a95c4d6747786ca120c065997f
Alix Field <32112823+kiaraBot@users.noreply.github.com>
2020-01-27 20:22:53 -0700

    Assets

---
+++ Assets/pinkBallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class pinkBallController : MonoBehaviour
+{
+
+    private float maxSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Pink Ball Instantiated");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody2D rbPlayer = GetComponent<Rigidbody2D>();
+
+        rbPlayer.velocity = new Vector2(Input.GetAxis("Horizontal") * maxSpeed, Input.GetAxis("Vertical") * maxSpeed);
+    }
+}
